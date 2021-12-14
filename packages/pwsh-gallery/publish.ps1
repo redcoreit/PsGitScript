0887763e15f53924acf658f4b122eb3212abd197
Roland Halbaksz <redcoreit@gmail.com>
2021-12-14 07:50:32 +0100

    Publish script improved.

--- packages/pwsh-gallery/publish.ps1
+++ packages/pwsh-gallery/publish.ps1
@@ -3,3 +3,4 @@ $apiKey = Read-Host -Prompt "Enter your API key" -AsSecureString
 Test-ModuleManifest -Path .\PsGitScript.psd1
 Invoke-ScriptAnalyzer -Path .\PsGitScript.psm1
 Publish-Module -Path . -NuGetApiKey $apiKey -Verbose
+Update-Module PsGitScript -Force
